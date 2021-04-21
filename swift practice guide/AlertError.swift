2143346ce6a82313f2a95cf67202da262de5c2ce
riku3 <takarakuten62@gmail.com>
2021-04-22 08:53:55 +0900

    extention エラーアラート

--- swift practice guide/AlertError.swift
+++ swift practice guide/AlertError.swift
@@ -5,4 +5,38 @@
 //  Created by riku on 2021/04/22.
 //
 
-import Foundation
+import UIKit
+
+enum WebAPIError: Error {
+    case connectionError
+    case fatalError
+    
+    var title: String {
+        switch self {
+        case .connectionError:
+            return "通信エラー"
+        case .fatalError:
+            return "致命的エラー"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .connectionError:
+            return localizedDescription + "再試行してください"
+        case .fatalError:
+            return "サポート窓口に連絡してください"
+        }
+    }
+}
+
+extension UIAlertController {
+    convenience init(webAPIError: WebAPIError) {
+        self.init(title: webAPIError.title,
+                  message: webAPIError.message,
+                  preferredStyle: .alert)
+    }
+}
+
+let error = WebAPIError.connectionError
+let alertController = UIAlertController(webAPIError: error)
