d761ecc78d1b1a3f1db3017e048851dc05f8d5aa
riku3 <takarakuten62@gmail.com>
2021-04-22 08:08:52 +0900

    Initial Commit

---
+++ swift practice guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift practice guide
+//
+//  Created by riku on 2021/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
